7152adb9b88e35cb9a7fec8020799a3f1d0cc69b
Utkarsh0119 <ussalunke20@gmail.com>
2026-02-05 06:13:15 +0000

    feat : chatbot added response file

---
+++ backend/src/main/java/com/portfolio/config/chatbot/ChatResponse.java
@@ -0,0 +1,19 @@
+package com.example.chatbotfeature.controller;
+
+public class ChatResponse {
+    private String response;
+
+    // Constructor
+    public ChatResponse(String response) {
+        this.response = response;
+    }
+
+    // Getter and Setter
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
